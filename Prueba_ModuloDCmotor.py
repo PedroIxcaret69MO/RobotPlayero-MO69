8491680512c5313acb00b49a3d77d7c68ebd8aa3
Pedro Ixcaret R.A <105024902+PedroIxcaret69MO@users.noreply.github.com>
2022-05-08 22:00:49 -0500

    Update Prueba_ModuloDCmotor.py

--- Prueba_ModuloDCmotor.py
+++ Prueba_ModuloDCmotor.py
@@ -4,8 +4,8 @@ from time import sleep
 
 frequency = 15000
 
-rpwm_d = PWM(Pin(12))
-lpwm_d = PWM(Pin(13))
+rpwm_d = PWM(Pin(16))
+lpwm_d = PWM(Pin(17))
 rpwm_i = PWM(Pin(14))
 lpwm_i = PWM(Pin(15))
 
@@ -16,12 +16,18 @@ lpwm_i.freq(frequency)
 
 dc_motor = DCMOTOR(rpwm_d, lpwm_d, rpwm_i, lpwm_i, min_duty=0, max_duty=65535)
 
-dc_motor.forward(50)
+dc_motor.forward(100)
 sleep(2)
 dc_motor.stop()
 sleep(3)
 dc_motor.backwards(100)
 sleep(2)
-dc_motor.forward(5)
-sleep(5)
+dc_motor.stop()
+sleep(3)
+dc_motor.left(100)
+sleep(2)
+dc_motor.stop()
+sleep(3)
+dc_motor.right(100)
+sleep(2)
 dc_motor.stop()
